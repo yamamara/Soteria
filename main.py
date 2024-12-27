fdc17a1f3f230666558b6dfd364416af81a5c1a9
yamamara <yamamrtyu@gmail.com>
2024-12-26 20:01:05 -0500

    Add logger + uptime

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 import logging
 import os
+import time
 from logging.handlers import TimedRotatingFileHandler
 
 import cv2
 from ultralytics import YOLO
 
-# Main camera hardware instance
+start_time = time.time()
 capture = cv2.VideoCapture(0)
-model = YOLO("yolov10x.pt")
-
+model = YOLO("yolo11x.pt")
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logFormatter = logging.Formatter("[%(asctime)s] [%(levelname)s/%(name)s] %(message)s")
 
-if not os.path.exists("log"):
-    os.makedirs("log")
 
-# Makes new log file at midnight
-file_handler = TimedRotatingFileHandler("log/soteria.log", when="midnight", interval=1, backupCount=30)
-file_handler.suffix = "%m-%d-%Y"
-file_handler.setFormatter(logFormatter)
+def elapsed_time_with_unit():
+    elapsed_time = time.time() - start_time
 
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(logFormatter)
+    if elapsed_time > 86400:
+        return f"{int(elapsed_time / 86400)} day(s)"
+    elif elapsed_time > 3600:
+        return f"{int(elapsed_time / 3600)} hour(s)"
+    elif elapsed_time > 60:
+        return f"{int(elapsed_time / 60)} minute(s)"
+    else:
+        return f"{int(elapsed_time)} seconds"
 
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+
+def setup_logger():
+    logger.setLevel(logging.DEBUG)
+    logFormatter = logging.Formatter("[%(asctime)s] [%(levelname)s/%(name)s] %(message)s")
+
+    if not os.path.exists("log"):
+        os.makedirs("log")
+
+    file_handler = TimedRotatingFileHandler("log/soteria.log", when="midnight", interval=1, backupCount=30)
+    file_handler.suffix = "%m-%d-%Y"
+    file_handler.setFormatter(logFormatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logFormatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 
 def predict(chosen_model, image, conf):
@@ -33,36 +48,54 @@ def predict(chosen_model, image, conf):
     rectangle_thickness = 2
     text_thickness = 1
 
-    # Draws labeled box on cv2 window for every prediction result
     for result in results:
         for box in result.boxes:
-            cv2.rectangle(image, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
-                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
-            cv2.putText(image, f"{result.names[int(box.cls[0])]}",
-                        (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+            cv2.rectangle(
+                image,
+                (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                (int(box.xyxy[0][2]), int(box.xyxy[0][3])),
+                (255, 0, 0),
+                rectangle_thickness
+            )
+
+            cv2.putText(
+                image,
+                f"{result.names[int(box.cls[0])]} - {(float(result.boxes.conf[0]) * 100):.2f}%",
+                (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                cv2.FONT_HERSHEY_PLAIN,
+                1,
+                (255, 0, 0),
+                text_thickness
+            )
 
     return image
 
 
-if not capture.isOpened():
-    logger.error("Unable to open camera")
+def main():
+    setup_logger()
+    stream_open = True
 
-while True:
-    frame_available, frame = capture.read()
+    if not capture.isOpened():
+        logger.error("Unable to open camera")
+        exit(1)
 
-    if not frame_available:
-        break
+    while stream_open:
+        frame_available, frame = capture.read()
 
-    processed_image = predict(model, frame, conf=0.5)
-    cv2.imshow("Webcam", processed_image)
+        if not frame_available:
+            break
 
-    # Captures keyboard input
-    key = cv2.waitKey(1)
+        processed_image = predict(model, frame, conf=0.5)
+        cv2.imshow("Webcam", processed_image)
+        key = cv2.waitKey(1)
 
-    # Closes window when "esc" key pressed
-    if key == 27:
-        break
+        if key == 27 or cv2.getWindowProperty('Webcam', cv2.WND_PROP_VISIBLE) == 0.0:
+            stream_open = False
 
-capture.release()
-cv2.destroyAllWindows()
+    capture.release()
+    cv2.destroyAllWindows()
+    # logger.info("Uptime: " + elapsed_time_with_unit())
+
+
+if __name__ == "__main__":
+    main()
